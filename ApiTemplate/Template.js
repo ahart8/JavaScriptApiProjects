4f68aa801445e57c4420aba859aa001cdacb0d61
ahart8 <121808111+ahart8@users.noreply.github.com>
2024-01-16 08:30:58 -0500

    Check in
    This should be the final queries and progam to run for the record deltions

--- ApiTemplate/Template.js
+++ ApiTemplate/Template.js
@@ -10,9 +10,6 @@ There are many other options, and for more complicated projects I use xhr2, but 
 https://axios-http.com/docs/intro
 */
 const axios = require('axios'); 
-
-
-
 const dbid = 'lcs-itg12'; //Company code to make this more reusable.
 
 /*
@@ -20,15 +17,11 @@ I am just grabbing the token from the test client dbid.api.rentmanager.com
 You would write auth and stuff normaly, but for quick API needs it saves me a few minutes of annoyance.
 */
 const token = 'cb39UhOOeoI5Cm26iQSezS_nYI-z7BJ5eLH_gtRnFVm2D6_dttxmZ0NM4da65UdJ_ll5SF9Couqk4lHYfOsCAxh9LHtZzmcpKsS1LET3JAU='; 
-
 const prospectIds = [1,2,3,4,5]; //Replace with an array of prospectIds. Once again we could pull data from RM, but this saves us time
-
 /*
-
 This is the actual main function that runs through each id and attempts to delete it.
 Its declared with the async tag because it is using async/await. https://javascript.info/async-await
 Promises and all of that is a very complicated topic and using async/await solves many of the issues faster than writing more optimized code.
-
 */
 async function main(){
 
